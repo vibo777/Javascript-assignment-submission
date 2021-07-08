5e5199a54a219fd0538f4f514e1ca89fe063f151
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-07-08 16:37:33 +0530

    Day 1 assignment submitted

---
+++ Assignment_day1.js
@@ -0,0 +1,10 @@
+// Question : Write a program in javascript to print five star's in a row
+//            * * * * *   
+
+// Solution :  
+s = ""
+for(i=1;i<=5;i++)   
+{
+   s = s + "* "     
+}
+console.log(s);
