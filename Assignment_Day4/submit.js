0cfd8baf3c415167e37ab10387d82755a2639a29
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-07-13 17:27:15 +0530

    Assignment_day4 submitted

---
+++ Assignment_Day4/submit.js
@@ -0,0 +1,8 @@
+function action(){
+
+    var input=document.getElementById("text").value;
+    console.log(text);
+    document.getElementById("msg").innerHTML="Hello "+input;
+}
+var btn=document.getElementById("btn");
+ 
